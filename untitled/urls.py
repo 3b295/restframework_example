c85c03ba5b4283f8fa69949f66398f929c5d9851
xiong weiyi <xwystz@gmail.com>
2018-04-19 17:33:33 +0800

    feat: add schemas

--- untitled/urls.py
+++ untitled/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mall.urls')),
     re_path(r'^api-auth/', include('rest_framework.urls')),
+    path(r'schema/', schema_view),
 ]
